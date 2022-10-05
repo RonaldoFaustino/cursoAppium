0998421b40c553e2b92204f7f9adee5d8d987737
RonaldoFaustino <ohnirucse@msn.com>
2022-10-04 21:33:05 -0300

    Criando DriverFactory

---
+++ src/main/java/org/example/appium/core/DriverFactory.java
@@ -0,0 +1,68 @@
+package org.example.appium.core;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.aspectj.lang.annotation.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class DriverFactory {
+
+    private static AndroidDriver<MobileElement> driverMobile;
+
+
+    public static AndroidDriver<MobileElement> getDriver(){
+        if (driverMobile == null){
+            createDriver();
+        }
+
+        return driverMobile;
+    }
+
+
+    private static void createDriver() {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("appium:deviceName", "emulator");
+        desiredCapabilities.setCapability("appium:automationName", "uiautomator2");
+        desiredCapabilities.setCapability("locationServicesAuthorized", true);
+        //desiredCapabilities.setCapability("appium:appPackage", "com.google.android.calculator");
+        //desiredCapabilities.setCapability("appium:appActivity", "com.android.calculator2.Calculator");
+        //desiredCapabilities.setCapability("noReset",true);
+        desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\cursoAppium\\src\\test\\resources\\apks\\CTAppium_1_2.apk");
+        desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
+        desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
+        desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
+        desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
+
+        desiredCapabilities.setCapability("appWaitPackage", "com.google.android.permissioncontroller");
+        desiredCapabilities.setCapability("appWaitActivity", "com.android.packageinstaller.permission.ui.ReviewPermissionsActivity");
+
+        try {
+            driverMobile = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        driverMobile.findElement(By.xpath("//android.widget.Button[@text='Continue']")).click();
+        try {
+            Thread.sleep(3000);
+            driverMobile.findElement(By.xpath("//*[@text='OK']")).click();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driverMobile.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    public static void killDriver(){
+        if (driverMobile != null){
+            driverMobile.quit();
+            driverMobile = null;
+        }
+    }
+
+}
--- src/test/java/org/example/appium/FormularioTest.java
+++ src/test/java/org/example/appium/FormularioTest.java
@@ -4,6 +4,7 @@ import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.example.appium.core.DriverFactory;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -22,37 +23,9 @@ public class FormularioTest {
 
         @Before
         public void setUp() throws MalformedURLException {
-            InicializarAppium();
+            driverMobile = DriverFactory.getDriver();
         }
 
-    private void InicializarAppium() throws MalformedURLException {
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setCapability("platformName", "Android");
-        desiredCapabilities.setCapability("appium:deviceName", "emulator");
-        desiredCapabilities.setCapability("appium:automationName", "uiautomator2");
-        desiredCapabilities.setCapability("locationServicesAuthorized", true);
-        //desiredCapabilities.setCapability("appium:appPackage", "com.google.android.calculator");
-        //desiredCapabilities.setCapability("appium:appActivity", "com.android.calculator2.Calculator");
-        //desiredCapabilities.setCapability("noReset",true);
-        desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\cursoAppium\\src\\test\\resources\\apks\\CTAppium_1_2.apk");
-        desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
-        desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
-        desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
-        desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
-
-        desiredCapabilities.setCapability("appWaitPackage", "com.google.android.permissioncontroller");
-        desiredCapabilities.setCapability("appWaitActivity", "com.android.packageinstaller.permission.ui.ReviewPermissionsActivity");
-
-        driverMobile = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
-        driverMobile.findElement(By.xpath("//android.widget.Button[@text='Continue']")).click();
-        try {
-            Thread.sleep(3000);
-            driverMobile.findElement(By.xpath("//*[@text='OK']")).click();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        driverMobile.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
 
     @Test
         public void devePreencherCampoTexto() {
@@ -153,7 +126,7 @@ public class FormularioTest {
 
     @After
     public void tearDown(){
-        driverMobile.quit();
+       DriverFactory.killDriver();
     }
 
 
